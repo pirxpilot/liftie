0a74bb67b37ed3a2632f8b7ef0f990e1b36e8a51
Damian Krzeminski <dkrzemin@gmail.com>
2012-12-23 16:21:48 -0800

    Calculate and display stats for each resort

---
+++ test/stats.js
@@ -0,0 +1,47 @@
+var assert = require('assert');
+var stats = require('../lib/stats');
+
+/*global describe, it*/
+
+describe('stats', function() {
+  it('should report 0 for empty', function() {
+    assert.deepEqual(stats([]), {
+      open: 0,
+      closed: 0,
+      hold: 0,
+      scheduled: 0
+    });
+  });
+  it('should count all status', function() {
+    var status = {
+      'n1': 'open',
+      'n2': 'open',
+      'n3': 'closed',
+      'n4': 'open',
+      'n5': 'open',
+      'n6': 'scheduled',
+      'n7': 'open',
+      'n8': 'closed',
+      'n9': 'closed',
+      'n11': 'hold',
+      'n12': 'hold',
+      'n13': 'closed',
+      'n14': 'scheduled',
+      'n17': 'scheduled'
+    };
+    status = Object.keys(status).map(function(key) {
+      return {
+        name: key,
+        status: status[key]
+      };
+    });
+
+    assert.deepEqual(stats(status), {
+      open: 5,
+      closed: 4,
+      hold: 2,
+      scheduled: 3
+    });
+
+  });
+});
