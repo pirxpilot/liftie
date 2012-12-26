5f91883df8fbccc5d0091fb67a9fc70a24cddf5f
Damian Krzeminski <dkrzemin@gmail.com>
2012-12-25 21:24:28 -0800

    Add Heavenly
    http://www.skiheavenly.com

---
+++ test/resorts/heavenly.js
@@ -0,0 +1,42 @@
+var assert = require('assert');
+var fs = require('fs');
+var parser = require('../../lib/parser');
+var parseHeavenly = require('../../lib/resorts/heavenly').parse;
+
+/*global describe, it */
+describe('parse heavenly', function() {
+
+  it('should return lift status', function(done) {
+    var stream = fs.createReadStream(__dirname + '/example/heavenly.html');
+    stream.on('error', done);
+    stream.pipe(parser(parseHeavenly, function(err, status) {
+      var expected = {
+        'Aerial Tram': 'open',
+        'Big Easy': 'open',
+        'Boulder': 'open',
+        'Canyon Express': 'open',
+        'Comet Express': 'open',
+        'Dipper Express': 'open',
+        'First Ride': 'open',
+        'Galaxy': 'open',
+        'Gondola': 'open',
+        'Groove': 'open',
+        'Gunbarrel Express ': 'open',
+        'Mott Canyon': 'open',
+        'North Bowl': 'open',
+        'Olympic Express': 'open',
+        'Patsy\'s': 'open',
+        'Powderbowl Express': 'open',
+        'Sky Express': 'open',
+        'Stagecoach Express': 'open',
+        'Surface Lifts CA (3)': 'open',
+        'Surface Lifts NV (3)': 'open',
+        'Tamarack Express': 'open',
+        'Tubing Lift (skiing and riding only)': 'open',
+        'World Cup': 'closed'
+      };
+      assert.deepEqual(status, expected);
+      done(err);
+    }));
+  });
+});
