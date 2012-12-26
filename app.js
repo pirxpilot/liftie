bf416c7a730853a2553132d70b1b304b1bdd43e3
Damian Krzeminski <dkrzemin@gmail.com>
2012-12-26 13:27:40 -0800

    Simple status REST API
    GET /resort/<resortname>
    
    Returns a json object which looks like this:
    
    {
        stats: {
            open: 3,
            closed: 0,
            scheduled: 0,
            hold: 0
        },
        href: 'http://www.ski-acme.com/link/to/lift/status',
        name: 'Ski Acme',
        status: {
            'Gondola': 'open',
            'Express': 'open',
            'Tram': 'open'
        }
    }

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+app.get('/api/resort/:resort', routes.api);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
