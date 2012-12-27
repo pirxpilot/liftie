57bc690292a86af0765b00261318def78a59fae6
Damian Krzeminski <dkrzemin@gmail.com>
2012-12-27 07:43:57 -0800

    Add Homewood Mountain support
    http://www.skihomewood.com/mountain/snow-report
    
    Looks like exact copy of Alpine Meadows page

---
+++ test/resorts/homewood.js
@@ -0,0 +1,25 @@
+var assert = require('assert');
+var fs = require('fs');
+var parser = require('../../lib/parser');
+var parse = require('../../lib/resorts/alpine').parse;
+
+/*global describe, it */
+describe('parse homewood', function() {
+
+  it('should return lift status', function(done) {
+    var stream = fs.createReadStream(__dirname + '/example/homewood.html');
+    stream.on('error', done);
+    stream.pipe(parser(parse, function(err, status) {
+      var expected = {
+        'Madden Chair': 'open',
+        'Quail Chair': 'open',
+        'Ellis Chair': 'open',
+        'Old Homewood Express': 'open',
+        'Critter Canyon Surface Lift': 'open',
+        'Happy Platter &amp; Alpine Platter S': 'open'
+      };
+      assert.deepEqual(status, expected);
+      done(err);
+    }));
+  });
+});
