d7c32d696c0b3bfee901a304c9917f8c9ae22f0b
Damian Krzeminski <dkrzemin@gmail.com>
2012-12-27 22:59:31 -0800

    Added Mount Rose ski resort
    http://www.skirose.com

---
+++ test/resorts/mtrose.js
@@ -0,0 +1,26 @@
+var assert = require('assert');
+var fs = require('fs');
+var parser = require('../../lib/parser');
+var parse = require('../../lib/resorts/mtrose').parse;
+
+/*global describe, it */
+describe('parse mtrose', function() {
+
+  it('should return lift status', function(done) {
+    var stream = fs.createReadStream(__dirname + '/example/mtrose.html');
+    stream.on('error', done);
+    stream.pipe(parser(parse, function(err, status) {
+      var expected = {
+        'Northwest Magnum 6': 'scheduled',
+        'Blazing Zephyr 6 ': 'scheduled',
+        'Chuter': 'scheduled',
+        'Lakeview': 'scheduled',
+        'Galena': 'scheduled',
+        'Ponderosa': 'scheduled',
+        'Flying Jenny': 'scheduled'
+      };
+      assert.deepEqual(status, expected);
+      done(err);
+    }));
+  });
+});
