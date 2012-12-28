824982b602ecfadf5b7947f5f6513b38037a6234
Damian Krzeminski <dkrzemin@gmail.com>
2012-12-27 23:53:58 -0800

    Added Northstar California ski resort
    http://www.northstarattahoe.com/

---
+++ test/resorts/northstar.js
@@ -0,0 +1,39 @@
+var assert = require('assert');
+var fs = require('fs');
+var parser = require('../../lib/parser');
+var parse = require('../../lib/resorts/northstar').parse;
+
+/*global describe, it */
+describe('parse northstar', function() {
+
+  it('should return lift status', function(done) {
+    var stream = fs.createReadStream(__dirname + '/example/northstar.html');
+    stream.on('error', done);
+    stream.pipe(parser(parse, function(err, status) {
+      var expected = {
+        'Promised Land Express': 'open',
+        'Backside Express': 'open',
+        'Martis Camp Express': 'open',
+        'Lookout Link': 'open',
+        'Vista Express': 'open',
+        'Comstock Express': 'open',
+        'Rendezvous': 'open',
+        'Arrow Express': 'open',
+        'The Big Easy': 'open',
+        'Tahoe Zephyr Express': 'open',
+        'Ursa Major': 'open',
+        'Big Dipper': 'open',
+        'Little Dipper': 'closed',
+        'Tubing Tow': 'open',
+        'Pegasus': 'open',
+        'Big Springs Express': 'open',
+        'Highlands Gondola': 'open',
+        'Village Express': 'open',
+        'Orion\'s Belt': 'open',
+        'Timberline Triple': 'closed'
+      };
+      assert.deepEqual(status, expected);
+      done(err);
+    }));
+  });
+});
