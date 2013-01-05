7133b13ff4621592bc3f23f36e07203781dcf1ec
Damian Krzeminski <dkrzemin@gmail.com>
2013-01-05 08:24:26 -0800

    Park City
    http://www.parkcitymountain.com

---
+++ test/resorts/parkcity.js
@@ -0,0 +1,35 @@
+var assert = require('assert');
+var fs = require('fs');
+var parser = require('../../lib/parser');
+var parse = require('../../lib/resorts/parkcity').parse;
+
+/*global describe, it */
+describe('parse parkcity', function() {
+
+  it('should return lift status', function(done) {
+    var stream = fs.createReadStream(__dirname + '/example/parkcity.html');
+    stream.on('error', done);
+    stream.pipe(parser(parse, function(err, status) {
+      var expected = {
+        '3 Kings': 'open',
+        Crescent: 'open',
+        Eagle: 'open',
+        'First Time': 'open',
+        Jupiter: 'open',
+        Eaglet: 'open',
+        'King Con': 'open',
+        'Silver Star': 'open',
+        'McConkey\'s': 'open',
+        Pioneer: 'open',
+        PayDay: 'open',
+        Town: 'open',
+        Bonanza: 'open',
+        Silverlode: 'open',
+        Motherlode: 'open',
+        Thaynes: 'open'
+      };
+      assert.deepEqual(status, expected);
+      done(err);
+    }));
+  });
+});
