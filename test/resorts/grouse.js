bbb95e9dbb4a3b2c6da71031d30376c49d0c036b
Damian Krzeminski <dkrzemin@gmail.com>
2013-01-06 09:31:16 -0800

    Grouse Mountain
    http://www.grousemountain.com

---
+++ test/resorts/grouse.js
@@ -0,0 +1,24 @@
+var assert = require('assert');
+var fs = require('fs');
+var parser = require('../../lib/parser');
+var parse = require('../../lib/resorts/grouse').parse;
+
+/*global describe, it */
+describe('parse grouse', function() {
+
+  it('should return lift status', function(done) {
+    var stream = fs.createReadStream(__dirname + '/example/grouse.html');
+    stream.on('error', done);
+    stream.pipe(parser(parse, function(err, status) {
+      var expected = {
+        'Olympic Express High-Speed Quad': 'open',
+        'Peak Quad': 'scheduled',
+        'Screaming Eagle High-Speed Quad': 'open',
+        'Greenway Quad': 'open',
+        'Magic Carpet': 'open'
+      };
+      assert.deepEqual(status, expected);
+      done(err);
+    }));
+  });
+});
