80b7dfbe4fb91126385fc7d73c12ceb964782ac0
Damian Krzeminski <dkrzemin@gmail.com>
2013-01-09 07:15:28 -0800

    Grand Targhee
    http://www.grandtarghee.com

---
+++ test/resorts/grand-targhee.js
@@ -0,0 +1,25 @@
+var assert = require('assert');
+var fs = require('fs');
+var parser = require('../../lib/parser');
+var parse = require('../../lib/resorts/grand-targhee').parse;
+
+/*global describe, it */
+describe('parse grand-targhee', function() {
+
+  it('should return lift status', function(done) {
+    var stream = fs.createReadStream(__dirname + '/example/grand-targhee.html');
+    stream.on('error', done);
+    stream.pipe(parser(parse, function(err, status) {
+      var expected = {
+        'Mary\'s Nipple': 'closed',
+        'Dreamcatcher': 'scheduled',
+        'Sacajawea': 'scheduled',
+        'Blackfoot': 'scheduled',
+        'Shoshone': 'scheduled',
+        'Papoose Conveyor': 'scheduled'
+      };
+      assert.deepEqual(status, expected);
+      done(err);
+    }));
+  });
+});
