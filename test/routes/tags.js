7c7daf7bee61309c62c7c51fe4e7cd14dcc69a1c
Damian Krzeminski <dkrzemin@gmail.com>
2013-01-09 20:26:52 -0800

    Convert tag names to URL friendly form
    URI for 'Lake Tahoe' is now 'lake-tahoe' etc.

--- test/routes/tags.js
+++ test/routes/tags.js
@@ -4,7 +4,7 @@ var tags = require('../../lib/routes/tags');
 /*global describe, it*/
 
 
-describe('sorter', function() {
+describe('tags', function() {
 
 	it('should create <all> tag with the names of all passed items', function() {
     function load(name) {
@@ -16,7 +16,7 @@ describe('sorter', function() {
     var tt = tags(names, load);
 
     assert.equal(Object.keys(tt).length, 1);
-    assert.deepEqual(tt.all, names);
+    assert.deepEqual(tt.all.members, names);
   });
 
   it('should classify object according to tags', function() {
@@ -36,11 +36,31 @@ describe('sorter', function() {
 
     var tt = tags(Object.keys(objs), load);
 
-    assert.deepEqual(tt.all, ['a', 'b', 'c', 'd']);
-    assert.deepEqual(tt.t1, ['a', 'c']);
-    assert.deepEqual(tt.t2, ['a', 'b', 'c']);
-    assert.deepEqual(tt.t3, ['a']);
+    assert.deepEqual(tt.all.members, ['a', 'b', 'c', 'd']);
+    assert.deepEqual(tt.t1.members, ['a', 'c']);
+    assert.deepEqual(tt.t2.members, ['a', 'b', 'c']);
+    assert.deepEqual(tt.t3.members, ['a']);
     assert.equal(Object.keys(tt).length, 4);
   });
 
+  it('should conver names to cannonical form', function() {
+    var objs = {
+      'a': 'Nice Tag,Another Tag',
+      'b': 'Another Tag',
+    };
+
+    function load(name){
+      return {
+        id: name,
+        tags: objs[name].split(',')
+      };
+    }
+
+    var tt = tags(Object.keys(objs), load);
+
+    assert.deepEqual(tt['nice-tag'].members, ['a']);
+    assert.deepEqual(tt['nice-tag'].label, 'Nice Tag');
+    assert.deepEqual(tt['another-tag'].members, ['a', 'b']);
+    assert.deepEqual(tt['another-tag'].label, 'Another Tag');
+  });
 });
