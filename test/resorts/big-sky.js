8ca82e027b4ac73414a34faa43b9c6b4bafeba6c
Damian Krzeminski <dkrzemin@gmail.com>
2013-01-11 07:31:44 -0800

    Big Sky
    http://www.bigskyresort.com

---
+++ test/resorts/big-sky.js
@@ -0,0 +1,39 @@
+var assert = require('assert');
+var fs = require('fs');
+var parser = require('../../lib/parser');
+var parse = require('../../lib/resorts/big-sky').parse;
+
+/*global describe, it */
+describe('parse big-sky', function() {
+
+  it('should return lift status', function(done) {
+    var stream = fs.createReadStream(__dirname + '/example/big-sky.html');
+    stream.on('error', done);
+    stream.pipe(parser(parse, function(err, status) {
+      var expected = {
+        'Adrian\'s Way Carpet': 'open',
+        'Bear Back Poma': 'open',
+        'Cascade': 'open',
+        'Challenger': 'open',
+        'Dakota': 'open',
+        'Explorer': 'open',
+        'Iron Horse': 'open',
+        'Little Thunder': 'open',
+        'Lone Moose': 'open',
+        'Lone Peak Tram': 'open',
+        'Lone Peak Triple': 'open',
+        'Middle Mountain Carpet': 'open',
+        'Pony Express': 'open',
+        'Ramcharger': 'open',
+        'Shedhorn': 'open',
+        'Small Fry Carpet': 'open',
+        'Southern Comfort': 'open',
+        'Swift Current': 'open',
+        'Thunder Wolf': 'open',
+        'White Otter': 'open'
+      };
+      assert.deepEqual(status, expected);
+      done(err);
+    }));
+  });
+});
