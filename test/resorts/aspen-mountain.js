8801ec63d1ed1f66f36f6ebcd02662ba056e305a
Damian Krzeminski <dkrzemin@gmail.com>
2013-01-11 08:03:33 -0800

    Aspen Mountain
    http://www.aspensnowmass.com/aspen-mountain

---
+++ test/resorts/aspen-mountain.js
@@ -0,0 +1,27 @@
+var assert = require('assert');
+var fs = require('fs');
+var parser = require('../../lib/parser');
+var parse = require('../../lib/resorts/aspen-mountain').parse;
+
+/*global describe, it */
+describe('parse aspen-mountain', function() {
+
+  it('should return lift status', function(done) {
+    var stream = fs.createReadStream(__dirname + '/example/aspen-mountain.html');
+    stream.on('error', done);
+    stream.pipe(parser(parse, function(err, status) {
+      var expected = {
+        'Gondola': 'open',
+        'Bell Mountain': 'closed',
+        'Ajax Express': 'open',
+        'Gents': 'open',
+        'Ruthies': 'open',
+        'FIS': 'open',
+        'Little Nell': 'closed',
+        'Shadow Mountain': 'open'
+      };
+      assert.deepEqual(status, expected);
+      done(err);
+    }));
+  });
+});
