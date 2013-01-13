d7af14f7a9ce5ed6a0ce4c7919166db46d082730
Damian Krzeminski <dkrzemin@gmail.com>
2013-01-13 08:17:24 -0800

    Sun Valley
    http://www.sunvalley.com

---
+++ test/resorts/sunvalley.js
@@ -0,0 +1,32 @@
+var assert = require('assert');
+var fs = require('fs');
+var parser = require('../../lib/parser');
+var parse = require('../../lib/resorts/sunvalley').parse;
+
+/*global describe, it */
+describe('parse sunvalley', function() {
+
+  it('should return lift status', function(done) {
+    var stream = fs.createReadStream(__dirname + '/example/sunvalley.html');
+    stream.on('error', done);
+    stream.pipe(parser(parse, function(err, status) {
+      var expected = {
+        'River Run': 'open',
+        'Christmas': 'open',
+        'Sunnyside': 'closed',
+        'Greyhawk': 'open',
+        'Challenger': 'open',
+        'Frenchman\'s': 'open',
+        'Lookout': 'open',
+        'Roundhouse Express': 'open',
+        'Lookout Express': 'open',
+        'Flying Squirrel': 'closed',
+        'Seattle Ridge': 'open',
+        'Cold Springs': 'open',
+        'Mayday': 'open'
+      };
+      assert.deepEqual(status, expected);
+      done(err);
+    }));
+  });
+});
