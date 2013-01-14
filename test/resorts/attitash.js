76028547a6eb31952dcb5a0072ff9f7665183937
Damian Krzeminski <dkrzemin@gmail.com>
2013-01-13 18:32:56 -0800

    Attitash
    http://www.attitash.com/snow-report.html

---
+++ test/resorts/attitash.js
@@ -0,0 +1,30 @@
+var assert = require('assert');
+var fs = require('fs');
+var parser = require('../../lib/parser');
+var parse = require('../../lib/resorts/attitash').parse;
+
+/*global describe, it */
+describe('parse attitash', function() {
+
+  it('should return lift status', function(done) {
+    var stream = fs.createReadStream(__dirname + '/example/attitash.html');
+    stream.on('error', done);
+    stream.pipe(parser(parse, function(err, status) {
+      var expected = {
+        'Learning Center Triple': 'open',
+        'Snowbelt': 'open',
+        'Abenaki Quad': 'open',
+        'Kachina Triple': 'open',
+        'Flying Bear': 'open',
+        'SnowTow': 'closed',
+        'Top Notch Double': 'closed',
+        'Flying Yankee': 'open',
+        'Summit Triple': 'open',
+        'East Double': 'open',
+        'West Double': 'closed'
+      };
+      assert.deepEqual(status, expected);
+      done(err);
+    }));
+  });
+});
