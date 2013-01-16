11e190a08ca1fc7bebd6a4718f987e4be576dc55
Damian Krzeminski <dkrzemin@gmail.com>
2013-01-16 08:35:02 -0800

    Loveland
    http://www.skiloveland.com

---
+++ test/resorts/loveland.js
@@ -0,0 +1,27 @@
+var assert = require('assert');
+var fs = require('fs');
+var parser = require('../../lib/parser');
+var parse = require('../../lib/resorts/loveland').parse;
+
+/*global describe, it */
+describe('parse loveland', function() {
+
+  it('should return lift status', function(done) {
+    var stream = fs.createReadStream(__dirname + '/example/loveland.html');
+    stream.on('error', done);
+    stream.pipe(parser(parse, function(err, status) {
+      var expected = {
+        'Chair 2': 'open',
+        'Chair 9': 'closed',
+        'Chair 8': 'closed',
+        'Chair 7': 'open',
+        'Chair 6': 'open',
+        'Chair 4': 'open',
+        'Chair 3': 'open',
+        'Chair 1': 'open'
+      };
+      assert.deepEqual(status, expected);
+      done(err);
+    }));
+  });
+});
