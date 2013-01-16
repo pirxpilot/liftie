4ad1cffe03881353f809b036cdb0ecdf9097ddb8
Damian Krzeminski <dkrzemin@gmail.com>
2013-01-16 15:44:22 -0800

    Sugar Bowl
    http://www.sugarbowl.com

---
+++ test/resorts/sugarbowl.js
@@ -0,0 +1,31 @@
+var assert = require('assert');
+var fs = require('fs');
+var parser = require('../../lib/parser');
+var parse = require('../../lib/resorts/sugarbowl').parse;
+
+/*global describe, it */
+describe('parse sugarbowl', function() {
+
+  it('should return lift status', function(done) {
+    var stream = fs.createReadStream(__dirname + '/example/sugarbowl.html');
+    stream.on('error', done);
+    stream.pipe(parser(parse, function(err, status) {
+      var expected = {
+        'Lincoln Express': 'open',
+        'Judah Express': 'open',
+        'Disney Express': 'open',
+        'Summit Chair': 'open',
+        'Christmas Tree Express': 'open',
+        'Crow\'s Nest': 'closed',
+        'Jerome Hill Express': 'open',
+        'Meadow': 'closed',
+        'White Pine': 'open',
+        'The Flume': 'open',
+        'Nob Hill': 'open',
+        'Gondola': 'open'
+      };
+      assert.deepEqual(status, expected);
+      done(err);
+    }));
+  });
+});
