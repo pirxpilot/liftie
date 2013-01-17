58da509602d10f21d682e74613b4a2f55ed0bfcd
Damian Krzeminski <dkrzemin@gmail.com>
2013-01-16 17:37:55 -0800

    Kirkwood
    http://winter.kirkwood.com/site/mountain/snow-report

---
+++ test/resorts/kirkwood.js
@@ -0,0 +1,31 @@
+var assert = require('assert');
+var fs = require('fs');
+var parser = require('../../lib/parser');
+var parse = require('../../lib/resorts/kirkwood').parse;
+
+/*global describe, it */
+describe('parse kirkwood', function() {
+
+  it('should return lift status', function(done) {
+    var stream = fs.createReadStream(__dirname + '/example/kirkwood.html');
+    stream.on('error', done);
+    stream.pipe(parser(parse, function(err, status) {
+      var expected = {
+        'SnowKirk': 'scheduled',
+        'Caples Crest': 'scheduled',
+        'Iron Horse': 'scheduled',
+        'Sunrise': 'scheduled',
+        'Solitude': 'scheduled',
+        'Cornice Express': 'scheduled',
+        'TC Express': 'scheduled',
+        'Bunny': 'scheduled',
+        'Wagon Wheel': 'scheduled',
+        'The Reut': 'scheduled',
+        'Vista': 'scheduled',
+        'Covered Wagon': 'scheduled'
+      };
+      assert.deepEqual(status, expected);
+      done(err);
+    }));
+  });
+});
