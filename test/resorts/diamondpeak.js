0fa5121e0fb6f5a3bc59c23c53344a5df41a70b0
Damian Krzeminski <pirxpilot@code42day.com>
2013-01-22 17:57:19 -0800

    Diamond Peak
    http://diamondpeak.com

---
+++ test/resorts/diamondpeak.js
@@ -0,0 +1,26 @@
+var assert = require('assert');
+var fs = require('fs');
+var parser = require('../../lib/parser');
+var parse = require('../../lib/resorts/diamondpeak').parse;
+
+/*global describe, it */
+describe('parse diamondpeak', function() {
+
+  it('should return lift status', function(done) {
+    var stream = fs.createReadStream(__dirname + '/example/diamondpeak.html');
+    stream.on('error', done);
+    stream.pipe(parser(parse, function(err, status) {
+      var expected = {
+        'Crystal Express': 'open',
+        'Lakeview': 'open',
+        'Lodgepole': 'open',
+        'Red Fox': 'closed',
+        'Ridge': 'closed',
+        'Schoolhouse': 'open',
+        'Surface Lift': 'open'
+      };
+      assert.deepEqual(status, expected);
+      done(err);
+    }));
+  });
+});
