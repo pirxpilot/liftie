7613ca32f3bdd8dc7ee5bfb94fd6f48de86e038e
Damian Krzeminski <pirxpilot@code42day.com>
2013-01-29 09:07:37 -0800

    Angel Fire
    http://www.angelfireresort.com

---
+++ test/resorts/angel-fire.js
@@ -0,0 +1,26 @@
+var assert = require('assert');
+var fs = require('fs');
+var parser = require('../../lib/parser');
+var parse = require('../../lib/resorts/angel-fire').parse;
+
+/*global describe, it */
+describe('parse angel-fire', function() {
+
+  it('should return lift status', function(done) {
+    var stream = fs.createReadStream(__dirname + '/example/angel-fire.html');
+    stream.on('error', done);
+    stream.pipe(parser(parse, function(err, status) {
+      var expected = {
+        'Dreamcatcher': 'open',
+        'Lift #2': 'open',
+        'Chile Express': 'open',
+        'Lift #3': 'closed',
+        'Southwest Flyer': 'open',
+        'Ski School Wondercarpet': 'open',
+        'Tubing Wondercarpet': 'open'
+      };
+      assert.deepEqual(status, expected);
+      done(err);
+    }));
+  });
+});
