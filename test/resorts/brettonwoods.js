c4b2bb27ca2a8d4c60e19db1666f0b66488dde63
Damian Krzeminski <pirxpilot@code42day.com>
2013-02-01 21:39:09 -0800

    Bretton Woods

---
+++ test/resorts/brettonwoods.js
@@ -0,0 +1,29 @@
+var assert = require('assert');
+var fs = require('fs');
+var parser = require('../../lib/parser');
+var parse = require('../../lib/resorts/brettonwoods').parse;
+
+/*global describe, it */
+describe('parse brettonwoods', function() {
+
+  it('should return lift status', function(done) {
+    var stream = fs.createReadStream(__dirname + '/example/brettonwoods.html');
+    stream.on('error', done);
+    stream.pipe(parser(parse, function(err, status) {
+      var expected = {
+        'B-Lift Double': 'closed',
+        'Rosebrook Summit Express': 'open',
+        'Bethlehem Express': 'open',
+        'Telegraph T-Bar': 'closed',
+        'Fabyan\'s Express Triple': 'open',
+        'West Mountain Express': 'open',
+        'Learning Center Quad': 'open',
+        'Wonder Carpet': 'open',
+        'Red Carpet': 'open',
+        'Zephyr': 'open'
+      };
+      assert.deepEqual(status, expected);
+      done(err);
+    }));
+  });
+});
