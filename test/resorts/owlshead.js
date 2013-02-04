b6b5bfd382f6240f011a9021d7c32fa5fbcb490e
Damian Krzeminski <pirxpilot@code42day.com>
2013-02-03 21:41:51 -0800

    Owl's Head

---
+++ test/resorts/owlshead.js
@@ -0,0 +1,28 @@
+var assert = require('assert');
+var fs = require('fs');
+var parser = require('../../lib/parser');
+var parse = require('../../lib/resorts/owlshead').parse;
+
+/*global describe, it */
+describe('parse owlshead', function() {
+
+  it('should return lift status', function(done) {
+    var stream = fs.createReadStream(__dirname + '/example/owlshead.html');
+    stream.on('error', done);
+    stream.pipe(parser(parse, function(err, status) {
+      var expected = {
+        'Main Quad': 'open',
+        'Green Chair': 'closed',
+        'Little Quad': 'open',
+        'Little Red': 'closed',
+        'Black Chair': 'open',
+        'Lake Chair': 'open',
+        'Blue Chair': 'open',
+        'Panorama': 'closed',
+        'Magic Carpet': 'open'
+      };
+      assert.deepEqual(status, expected);
+      done(err);
+    }));
+  });
+});
