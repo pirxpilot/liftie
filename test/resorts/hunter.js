64de86d61007a4680f8e9ce9f94a0e47f2514112
Damian Krzeminski <pirxpilot@code42day.com>
2013-02-07 14:40:11 -0800

    Hunter Mountain

---
+++ test/resorts/hunter.js
@@ -0,0 +1,29 @@
+var assert = require('assert');
+var fs = require('fs');
+var parser = require('../../lib/parser');
+var parse = require('../../lib/resorts/hunter').parse;
+
+/*global describe, it */
+describe('parse hunter', function() {
+
+  it('should return lift status', function(done) {
+    var stream = fs.createReadStream(__dirname + '/example/hunter.html');
+    stream.on('error', done);
+    stream.pipe(parser(parse, function(err, status) {
+      var expected = {
+        'Kaatskill Flyer': 'open',
+        'B-Lift': 'open',
+        'C-Lift': 'open',
+        'D-Lift': 'closed',
+        'E-Lift': 'closed',
+        'F-Lift': 'scheduled',
+        'G-Lift': 'open',
+        'H-Lift': 'closed',
+        'Zephyr Express': 'open',
+        'Pony Lift': 'open'
+      };
+      assert.deepEqual(status, expected);
+      done(err);
+    }));
+  });
+});
