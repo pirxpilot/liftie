e844181efa1a1b80f28382c630bb2416816d3220
Natalia Kowalczyk <natalia.kowalczyk@gmail.com>
2013-10-20 20:14:52 -0400

    fix Mt Rose status for new season

--- test/resorts/mtrose.js
+++ test/resorts/mtrose.js
@@ -1,4 +1,4 @@
-var assert = require('assert');
+var should = require('should');
 var fs = require('fs');
 var parser = require('../../lib/parser');
 var parse = require('../../lib/resorts/mtrose');
@@ -11,15 +11,15 @@ describe('parse mtrose', function() {
     stream.on('error', done);
     stream.pipe(parser(parse, function(err, status) {
       var expected = {
-        'Northwest Magnum 6': 'scheduled',
-        'Blazing Zephyr 6 ': 'scheduled',
-        'Chuter': 'scheduled',
-        'Lakeview': 'scheduled',
-        'Galena': 'scheduled',
-        'Ponderosa': 'scheduled',
-        'Flying Jenny': 'scheduled'
+        'Northwest Magnum 6': 'closed',
+        'Blazing Zephyr 6 (Slide Bowl)': 'closed',
+        'Chuter': 'closed',
+        'Lakeview': 'closed',
+        'Galena': 'closed',
+        'Ponderosa': 'closed',
+        'Flying Jenny': 'closed'
       };
-      assert.deepEqual(status, expected);
+      status.should.eql(expected);
       done(err);
     }));
   });
