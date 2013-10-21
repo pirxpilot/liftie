edec8095ba71a32b23d7cfc46a2c94f756a0c012
Damian Krzeminski <pirxpilot@code42day.com>
2013-10-20 20:08:48 -0400

    fix Crested Butte for the new season

--- test/resorts/crested-butte.js
+++ test/resorts/crested-butte.js
@@ -1,4 +1,4 @@
-var assert = require('assert');
+var should = require('should');
 var fs = require('fs');
 var parser = require('../../lib/parser');
 var parse = require('../../lib/resorts/crested-butte');
@@ -11,23 +11,24 @@ describe('parse crested-butte', function() {
     stream.on('error', done);
     stream.pipe(parser(parse, function(err, status) {
       var expected = {
-        'Red Lady Express': 'open',
-        'Silver Queen Express': 'open',
-        'Gold Link Lift': 'open',
-        'Prospect Lift': 'open',
-        'East River Express': 'open',
+        'Red Lady Express': 'closed',
+        'Silver Queen Express': 'closed',
+        'Gold Link Lift': 'closed',
+        'Prospect Lift': 'closed',
+        'East River Express': 'closed',
         'Teocalli Lift': 'closed',
-        'Paradise Express': 'open',
-        'West Wall Lift': 'open',
+        'Paradise Express': 'closed',
+        'West Wall Lift': 'closed',
         'Twister Lift': 'closed',
         'North Face Lift': 'closed',
         'The High Lift': 'closed',
-        'Peachtree Lift': 'open',
-        'Painter Boy Lift': 'open',
-        'Aspen Magic Carpet': 'open',
-        'Pine Magic Carpet': 'open'
+        'Peachtree Lift': 'closed',
+        'Painter Boy Lift': 'closed',
+        'Aspen Magic Carpet': 'closed',
+        'Pine Magic Carpet': 'closed'
       };
-      assert.deepEqual(status, expected);
+      should.exist(status);
+      status.should.eql(expected);
       done(err);
     }));
   });
