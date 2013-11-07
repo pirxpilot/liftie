4a08bea9750c516795c43bb019c9a527e304a3e9
Damian Krzeminski <pirxpilot@code42day.com>
2013-11-07 17:40:28 -0500

    server side /stats rendering

--- test/lifts/stats.js
+++ test/lifts/stats.js
@@ -49,3 +49,53 @@ describe('stats', function() {
 
   });
 });
+
+describe('stats.summary', function() {
+  it('should calculate summary for empty stats', function() {
+    stats.summary([]).should.eql({
+      open: 0,
+      closed: 0,
+      hold: 0,
+      scheduled: 0,
+      percentage: {
+        open: 25,
+        closed: 25,
+        hold: 25,
+        scheduled: 25
+      }
+    });
+  });
+
+  it('should calculate summary for array of stats', function() {
+    stats.summary([{
+      open: 5,
+      closed: 4,
+      hold: 2,
+      scheduled: 3
+    },
+    {
+      open: 10,
+      closed: 1
+    },
+    null,
+    {},
+    {
+      open: 1,
+      closed: 2,
+      hold: 3,
+      scheduled: 4
+    }]).should.eql({
+      open: 16,
+      closed: 7,
+      hold: 5,
+      scheduled: 7,
+      percentage: {
+        open: 45.7,
+        closed: 20,
+        hold: 14.2,
+        scheduled: 20
+      }
+    });
+  });
+
+});
