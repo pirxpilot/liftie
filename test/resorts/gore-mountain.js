4a0684bb60c4aac75ae48adf0ed31fe2a3a99cb3
Damian Krzeminski <pirxpilot@code42day.com>
2013-11-15 07:39:08 -0500

    add Gore Mountain resort

---
+++ test/resorts/gore-mountain.js
@@ -0,0 +1,35 @@
+var should = require('should');
+var fs = require('fs');
+var parser = require('../../lib/lifts/parser');
+var parse = require('../../lib/resorts/gore-mountain');
+
+/*global describe, it */
+describe('parse gore-mountain', function() {
+
+  it('should return lift status', function(done) {
+    var stream = fs.createReadStream(__dirname + '/example/gore-mountain.html');
+    stream.on('error', done);
+    stream.pipe(parser(parse, function(err, status) {
+      var expected = {
+        'Northwoods Gondola': 'closed',
+        'Topridge Triple': 'closed',
+        'Straight Brook Quad': 'closed',
+        'Sunway Chair': 'closed',
+        'Adirondack Express': 'closed',
+        'Burnt Ridge Quad': 'closed',
+        'High Peaks Chair': 'closed',
+        'North Quad': 'closed',
+        'J-Bar': 'closed',
+        'Bear Cub': 'closed',
+        'Greenway Conveyor': 'closed',
+        'The Snow Train': 'closed',
+        'Village Chair': 'closed',
+        'Hudson Chair': 'closed',
+        'Little Gore Tubing Tow': 'closed'
+      };
+      should.exist(status);
+      status.should.eql(expected);
+      done(err);
+    }));
+  });
+});
