fbc7f64926b511c8cc9381475f8d99b648df33f7
Natalia Kowalczyk <natalia.kowalczyk@gmail.com>
2013-12-03 21:29:58 -0500

    Add snow forecast to weather display

--- test/weather/opensnowapi.js
+++ test/weather/opensnowapi.js
@@ -30,7 +30,7 @@ describe('weather', function() {
         forecast.should.have.property('date').with.match(/\d{4}-\d{2}-\d{2}/);
         forecast.should.have.property('text').with.type('string');
         forecast.should.have.property('conditions').with.type('string');
-        forecast.should.have.property('snow').with.type('number');
+        forecast.should.have.property('snow').with.type('string');
         forecast.should.have.property('temperature').with.type('object');
         forecast.temperature.should.have.property('max').with.type('number');
         done(err);
