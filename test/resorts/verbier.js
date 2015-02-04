92ac044fb136f8f96a7ad0e6ea1fad24ea00a33e
WilliamHF <william@halff.fr>
2015-02-04 19:40:48 +0000

    Adding infosnow module for swiss resorts
    Adding more Swiss resorts with Infosnow module
    Can virtually add all resorts on infosnow.ch
    Added Verbier as an example

---
+++ test/resorts/verbier.js
@@ -0,0 +1,51 @@
+var should = require('should');
+var fs = require('fs');
+var parser = require('../../lib/lifts/parser');
+var parse = require('../../lib/resorts/verbier');
+
+/*global describe, it */
+describe('parse verbier', function() {
+
+  it('should return lift status', function(done) {
+    var stream = fs.createReadStream(__dirname + '/example/verbier.html');
+    stream.on('error', done);
+    stream.pipe(parser(parse, function(err, status) {
+      var expected = {
+        'La Chaux  -  Gentianes (Jumbo)': 'open',
+          'Tortin - Gentianes': 'open',
+          'Gentianes': 'open',
+          'Mont - Fort': 'open',
+          'Le Ch&acirc;ble - Verbier': 'open',
+          'Verbier - Ruinettes': 'open',
+          'Mayentzet': 'open',
+          'La Chaux-Express': 'open',
+          'Funispace': 'open',
+          'Les Attelas': 'open',
+          'La Chaux 2': 'open',
+          'Lac des Vaux 1': 'open',
+          'Lac des Vaux 2': 'open',
+          'Chassoure': 'open',
+          'Mont - Gel&eacute;': 'closed',
+          'Les Esserts': 'open',
+          'Les Moulins': 'open',
+          'Le Rouge': 'open',
+          'Verbier  -  Savoleyres': 'open',
+          'Savoleyres Sud': 'open',
+          'La Tournelle': 'open',
+          'Tournelle': 'open',
+          'La Tzoumaz  -  Savoleyres': 'open',
+          'Tapis Tzoumaz': 'open',
+          'Les Etablons': 'open',
+          'Taillay': 'open',
+          'Savoleyres Nord': 'open',
+          'Le Ch&acirc;ble - Mayens de Bruson': 'open',
+          'Moay': 'open',
+          'La Pasay': 'open',
+          'Le Grand - Tsai': 'open'
+              };
+      should.exist(status);
+      status.should.eql(expected);
+      done(err);
+    }));
+  });
+});
