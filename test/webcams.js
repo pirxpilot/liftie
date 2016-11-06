e41d664c51273baa006dc6c912e796d9c14a5acb
Damian Krzeminski <pirxpilot@code42day.com>
2016-11-06 08:13:12 -0800

    upgrade webcams.travel API
    new endpoint, new provider (Mashape), new (however still weird and non-standard) URL syntax
    
    see migration guide here:
    http://developers.webcams.travel/#webcams/migration-guide

--- test/webcams.js
+++ test/webcams.js
@@ -20,7 +20,7 @@ describe('webcams', function() {
     it('should return webcams for valid location', function(done) {
       webcams({
         counter: 1,
-        ll: [ -118.616810, 36.450008 ] // Mineral King
+        ll: [ 7.98, 46.54 ] // from API examples http://developers.webcams.travel/#webcams/examples
       }, function(err, webcams) {
         var webcam, mobile;
 
@@ -31,7 +31,7 @@ describe('webcams', function() {
 
         webcam = webcams[0];
 
-        webcam.should.have.property('name', 'Mineral King: Farewell Gap − Sequoia National Park');
+        webcam.should.have.property('name', 'Jungfraujoch: Top of Europe');
         webcam.should.have.property('source').with.startWith('http://www.webcams.travel/webcam');
         webcam.should.have.property('image').with.startWith('https://images.webcams.travel/preview/');
         webcam.should.have.property('notice', 'Webcam by <a href="http://webcams.travel" target="_blank">webcams.travel</a>');
@@ -39,7 +39,7 @@ describe('webcams', function() {
         webcam.should.have.property('mobile').with.type('object');
         mobile = webcam.mobile;
 
-        mobile.should.have.property('name', 'Mineral King: Farewell Gap − Sequoia National Park');
+        mobile.should.have.property('name', 'Jungfraujoch: Top of Europe');
         mobile.should.have.property('source').with.startWith('http://m.webcams.travel/webcam');
         mobile.should.have.property('image').with.startWith('https://images.webcams.travel/preview/');
         mobile.should.have.property('notice', 'Webcam by <a href="http://m.webcams.travel" target="_blank">webcams.travel</a>');
