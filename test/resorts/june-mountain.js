bd0af5336b0f59942f5f82886c94a6f3f8f4f958
Damian Krzeminski <pirxpilot@code42day.com>
2016-11-05 22:26:30 -0700

    add June Mountain resort

---
+++ test/resorts/june-mountain.js
@@ -0,0 +1,26 @@
+var should = require('should');
+var fs = require('fs');
+var parser = require('../../lib/lifts/parser');
+var parse = require('../../lib/resorts/june-mountain');
+
+/*global describe, it */
+describe('parse june-mountain', function() {
+
+  it('should return lift status', function(done) {
+    var stream = fs.createReadStream(__dirname + '/example/june-mountain.html');
+    stream.on('error', done);
+    stream.pipe(parser(parse, function(err, status) {
+      var expected = {
+        'J1': 'closed',
+        'J2': 'closed',
+        'J3': 'closed',
+        'J4': 'closed',
+        'J6': 'closed',
+        'J7': 'closed'
+      };
+      should.exist(status);
+      status.should.eql(expected);
+      done(err);
+    }));
+  });
+});
