2cafca708fa1f688ff576ce43b7bf3861c7f028a
Damian Krzeminski <pirxpilot@code42day.com>
2016-11-07 07:37:59 -0800

    fix Cannon resort

--- test/resorts/cannon.js
+++ test/resorts/cannon.js
@@ -11,15 +11,15 @@ describe('parse cannon', function() {
     stream.on('error', done);
     stream.pipe(parser(parse, function(err, status) {
       var expected = {
-        '70-Passenger Aerial Tramway': 'open',
-        'Brookside Triple Chair': 'open',
-        'Cannonball Express Quad Chair': 'open',
-        'Eagle Cliff Triple Chair': 'open',
-        'Magic Carpet': 'open',
-        'Peabody Express Quad Chair': 'open',
-        'Huckerbrook Handle Tow': 'open',
-        'Tuckerbrook Quad Chair': 'open',
-        'Zoomer Triple Chair': 'open',
+        '70-Passenger Aerial Tramway': 'closed',
+        'Brookside Triple Chair': 'closed',
+        'Cannonball Express Quad Chair': 'closed',
+        'Eagle Cliff Triple Chair': 'closed',
+        'Magic Carpet': 'closed',
+        'Peabody Express Quad Chair': 'closed',
+        'Huckerbrook Handle Tow': 'closed',
+        'Tuckerbrook Quad Chair': 'closed',
+        'Zoomer Triple Chair': 'closed',
         'Mittersill Double Chair': 'closed'
       };
       assert.deepEqual(status, expected);
