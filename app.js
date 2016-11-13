df6f86b3d2b3a80c436ab2383f07d433482e0274
Damian Krzeminski <pirxpilot@code42day.com>
2016-11-13 10:50:45 -0800

    fix default SITE_URL value

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var plugins = require('./lib/plugins');
 var app = module.exports = express();
 
 if (!process.env.SITE_URL) {
-  process.env.SITE_URL =  (app.get('env') === 'development')
+  process.env.SITE_URL =  (app.get('env') === 'production')
     ? 'https://liftie.info'
     : 'http://locahost:3000';
 }
