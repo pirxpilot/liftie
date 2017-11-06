3bf85b3787f4f6929496a7b297ed4024626a1597
Damian Krzeminski <pirxpilot@code42day.com>
2017-11-05 21:33:00 -0700

    add support for parse descriptor

--- test/resorts/49-degrees-north.js
+++ test/resorts/49-degrees-north.js
@@ -1,7 +1,7 @@
 var should = require('should');
 var fs = require('fs');
 var parser = require('../../lib/lifts/parser');
-var parse = require('../../lib/resorts/49-degrees-north');
+var parse = require('../../lib/lifts/parse')('49-degrees-north');
 
 /*global describe, it */
 describe('parse 49-degrees-north', function() {
