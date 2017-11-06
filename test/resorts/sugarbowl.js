5242a18a7e5254a243688b8844afaf8b0a996ed2
Damian Krzeminski <pirxpilot@code42day.com>
2017-11-05 21:40:44 -0700

    fix Sugarbowl for 2017/18 season

--- test/resorts/sugarbowl.js
+++ test/resorts/sugarbowl.js
@@ -11,18 +11,18 @@ describe('parse sugarbowl', function() {
     stream.on('error', done);
     stream.pipe(parser(parse, function(err, status) {
       var expected = {
-        'Lincoln Express': 'open',
-        'Judah Express': 'open',
-        'Disney Express': 'open',
+        'Lincoln Express': 'closed',
+        'Judah Express': 'closed',
+        'Disney Express': 'closed',
         'Summit Chair': 'closed',
-        'Christmas Tree Express': 'open',
-        'Crow\'s Nest': 'closed',
-        'Jerome Hill Express': 'open',
-        'Meadow': 'closed',
-        'White Pine': 'open',
-        'The Flume': 'open',
-        'Nob Hill': 'open',
-        'Gondola': 'open'
+        'Christmas Tree Express': 'closed',
+        'Crow\'s Peak': 'closed',
+        'Jerome Hill Express': 'closed',
+        'Village Tow': 'closed',
+        'White Pine (Beginner)': 'closed',
+        'Flume Carpet': 'closed',
+        'Nob Hill (Beginner)': 'closed',
+        'Gondola': 'closed'
       };
       assert.deepEqual(status, expected);
       done(err);
