7d83510d428557764087be19ce1a4e921c1edeae
Damian Krzeminski <pirxpilot@code42day.com>
2018-01-06 16:35:31 -0700

    fix Sunshine Village parser

--- test/resorts/sunshine-village.js
+++ test/resorts/sunshine-village.js
@@ -1,30 +1,24 @@
-var should = require('should');
-var fs = require('fs');
-var parser = require('../../lib/lifts/parser');
 var parse = require('../../lib/lifts/parse')('sunshine-village');
 
 /*global describe, it */
 describe('parse sunshine-village', function() {
 
-  it('should return lift status', function(done) {
-    var stream = fs.createReadStream(__dirname + '/example/sunshine-village.html');
-    stream.on('error', done);
-    stream.pipe(parser(parse, function(err, status) {
-      var expected = {
-        "Gondola": "open",
-        "Angel": "closed",
-        "Strawberry": "open",
-        "Standish": "open",
-        "Divide": "closed",
-        "Goat's Eye": "closed",
-        "Jackrabbit": "hold",
-        "Wolverine": "hold",
-        "Wawa": "open",
-        "Teepee Town": "closed"
-      };
-      should.exist(status);
-      status.should.eql(expected);
-      done(err);
-    }));
+  it('should return lift status', function() {
+    var data = require('./example/sunshine-village.json');
+    var expected = {
+      'Angel': 'open',
+      'Wolverine': 'open',
+      'Goat\'s Eye': 'open',
+      'Jackrabbit': 'open',
+      'Teepee Town': 'open',
+      'Divide': 'open',
+      'Standish': 'open',
+      'Strawberry': 'open',
+      'Gondola': 'open',
+      'Wawa': 'open',
+      'Mitey Mite': 'open',
+      'Kids Kampus': 'open'
+    };
+    parse(data).should.eql(expected);
   });
 });
