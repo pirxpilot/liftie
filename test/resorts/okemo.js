824c61e281fae4d25548be728a8663d8d594a43a
Damian Krzeminski <pirxpilot@code42day.com>
2018-03-12 17:46:49 -0600

    fix Okemo resort

--- test/resorts/okemo.js
+++ test/resorts/okemo.js
@@ -11,25 +11,26 @@ describe('parse okemo', function() {
     stream.on('error', done);
     stream.pipe(parser(parse, function(err, status) {
       var expected = {
-        'Solitude Express Quad': 'closed',
-        'Morning Star Triple': 'closed',
-        'Jackson Gore Express Quad': 'closed',
-        'Coleman Brook Express Quad': 'closed',
-        'Stargazer Carpet': 'closed',
+        'Solitude Express Quad': 'open',
+        'Morning Star Triple': 'open',
+        'Quantum Four': 'open',
+        'Coleman Brook Express Quad': 'open',
+        'Stargazer Carpet': 'open',
         'Orion\'s Belt Carpet': 'closed',
-        'Green Ridge Triple': 'closed',
-        'F-10 Carpet': 'closed',
+        'Green Ridge Triple': 'open',
+        'F-10 Carpet': 'open',
         'Starlight Carpet': 'closed',
         'The Pull': 'closed',
         'Black Ridge Triple': 'closed',
-        'Sunburst Six': 'closed',
-        'Sachem Quad': 'closed',
+        'Sunburst Six': 'open',
+        'Sachem Quad': 'open',
         'Glades Peak Quad': 'closed',
-        'South Face Express Quad': 'closed',
-        'South Ridge Quad A': 'closed',
+        'South Face Express Quad': 'open',
+        'South Ridge Quad A': 'open',
         'South Ridge Quad B': 'closed',
         'Skywalker Carpet': 'closed',
-        'Snow Star Carpet': 'closed'
+        'Snow Star Carpet': 'closed',
+        'Sunshine Quad': 'open'
       };
       should.exist(status);
       status.should.eql(expected);
