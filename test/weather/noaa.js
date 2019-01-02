7803617dcaa2d4c40ca4a7680dc9205be2a690d8
Damian Krzeminski <pirxpilot@furkot.com>
2019-01-02 15:03:01 -0700

    add support for noaa weather forecast
    API docs: https://www.weather.gov/documentation/services-web-api

---
+++ test/weather/noaa.js
@@ -0,0 +1,35 @@
+const should = require('should');
+const noaa = require('../../lib/weather/noaa');
+
+/*global describe, it*/
+
+describe('noaa', function() {
+
+  it('should return empty forecast if location is missing', function(done) {
+    noaa({
+      ll: [0, 0]
+    }, function(err, forecast) {
+      should.not.exist(err);
+      should.not.exist(forecast);
+      done();
+    });
+  });
+
+
+  it('should return forecast for valid location', function(done) {
+    noaa({
+      ll: [-72.7933, 43.6647] // Killington, VT
+    }, function(err, forecast) {
+      should.not.exist(err);
+      should.exist(forecast);
+      forecast.should.have.property('icon', ['icon-cloud', 'icon-sunny']);
+      forecast.should.have.property('date', '2019-01-02');
+      forecast.should.have.property('text', 'Partly sunny, with a high near 25. East wind around 3 mph.');
+      forecast.should.have.property('conditions', 'Partly Sunny');
+      forecast.should.have.property('temperature').with.type('object');
+      forecast.temperature.should.have.property('max', 25);
+      done(err);
+    });
+  });
+
+});
