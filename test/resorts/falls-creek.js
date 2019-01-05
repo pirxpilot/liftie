925df7b7f7b7034f0cc998471adea244365fb55e
Damian Krzeminski <pirxpilot@furkot.com>
2019-01-05 16:03:21 -0700

    fix falls-creek parser and test
    closes #24

--- test/resorts/falls-creek.js
+++ test/resorts/falls-creek.js
@@ -1,34 +1,26 @@
-const should = require('should');
-const { createReadStream } = require('fs');
-const parser = require('../../lib/lifts/parser');
-const parse = require('../../lib/lifts/parse')('falls-creek');
+var parse = require('../../lib/lifts/parse')('falls-creek');
 
 /*global describe, it */
 describe('parse falls-creek', function() {
 
-  it('should return lift status', function(done) {
-    const stream = createReadStream(`${__dirname}/example/falls-creek.html`);
-    stream.on('error', done);
-    stream.pipe(parser(parse, function(err, status) {
-      const expected = {
-        'HALLEYS COMET': 'closed',
-        'TOWERS CHAIR': 'closed',
-        'DROVERS DREAM CHAIR': 'closed',
-        'BOARD WALK': 'closed',
-        'MOUSE TRAP': 'closed',
-        'MONKEY BARS': 'closed',
-        'PETES TRAIN': 'closed',
-        'GULLY CHAIR': 'closed',
-        'EAGLE EXPRESS': 'closed',
-        'SCOTTS CHAIR': 'closed',
-        'RUINED CASTLE CHAIR': 'closed',
-        'SUMMIT CHAIR': 'closed',
-        'LAKESIDE POMA': 'closed',
-        'INTERNATIONAL POMA': 'closed',
-      };
-      should.exist(status);
-      status.should.eql(expected);
-      done(err);
-    }));
+  it('should return lift status', function() {
+    var data = require('./example/falls-creek.json');
+    var expected = {
+      'Halleys Comet': 'closed',
+      'Towers Chair': 'closed',
+      'Drovers Dream Chair': 'closed',
+      'Board Walk': 'closed',
+      'Mouse Trap': 'closed',
+      'Monkey Bars ': 'closed',
+      'Petes Train ': 'closed',
+      'Gully Chair': 'closed',
+      'Eagle Express': 'closed',
+      'Scotts Chair': 'closed',
+      'Ruined Castle Chair': 'closed',
+      'Summit Chair': 'closed',
+      'Lakeside Poma': 'closed',
+      'International Poma': 'closed'
+    };
+    parse(data).should.eql(expected);
   });
 });
