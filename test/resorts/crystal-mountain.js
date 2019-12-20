1f325d38420836b395008de4d47c84d17ea3a7bd
Damian Krzeminski <pirxpilot@furkot.com>
2019-12-20 13:28:53 +0100

    fix Crystal Mountain for the new season

--- test/resorts/crystal-mountain.js
+++ test/resorts/crystal-mountain.js
@@ -1,33 +1,23 @@
-var should = require('should');
-var fs = require('fs');
-var parser = require('../../lib/lifts/parser');
-var parse = require('../../lib/lifts/parse')('crystal-mountain');
+const assert = require('assert');
+const parse = require('../../lib/lifts/parse')('crystal-mountain');
 
 /*global describe, it */
 describe('parse crystal-mountain', function() {
 
-  it('should return lift status', function(done) {
-    var stream = fs.createReadStream(__dirname + '/example/crystal-mountain.html');
-    stream.on('error', done);
-    stream.pipe(parser(parse, function(err, status) {
-      var expected = {
-        'Gondola': 'closed',
-        'Chinook Express': 'closed',
-        'Discovery': 'closed',
-        'Forest Queen': 'closed',
-        'Gold Hills': 'closed',
-        'Green Valley': 'closed',
-        'Chair 6': 'closed',
-        'Northway': 'closed',
-        'Quicksilver': 'closed',
-        'Rainier Express': 'closed',
-        'Magic Carpet': 'closed',
-        'Sasquatch Park': 'closed',
-        'Southback': 'closed'
-      };
-      should.exist(status);
-      status.should.eql(expected);
-      done(err);
-    }));
+  it('should return lift status', function() {
+    const data = require('./example/crystal-mountain.json');
+    const expected = {
+      'Mt. Rainier Gondola': 'closed',
+      'Chinook Express': 'open',
+      'Discovery': 'open',
+      'Forest Queen': 'open',
+      'Gold Hills': 'closed',
+      'Green Valley': 'open',
+      'Chair 6': 'closed',
+      'Northway': 'closed',
+      'Quicksilver': 'open',
+      'Rainier Express': 'open'
+    };
+    assert.deepEqual(parse(data), expected);
   });
 });
