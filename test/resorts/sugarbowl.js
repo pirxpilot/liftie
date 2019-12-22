141da48c7d27325c1c9ca1cbf3d9abed96d621c1
Damian Krzeminski <pirxpilot@furkot.com>
2019-12-22 12:21:09 +0100

    fix Sugarbowl

--- test/resorts/sugarbowl.js
+++ test/resorts/sugarbowl.js
@@ -1,28 +1,29 @@
-var assert = require('assert');
-var fs = require('fs');
-var parser = require('../../lib/lifts/parser');
-var parse = require('../../lib/lifts/parse')('sugarbowl');
+const assert = require('assert');
+const fs = require('fs');
+const parser = require('../../lib/lifts/parser');
+const parse = require('../../lib/lifts/parse')('sugarbowl');
 
 /*global describe, it */
 describe('parse sugarbowl', function() {
 
   it('should return lift status', function(done) {
-    var stream = fs.createReadStream(__dirname + '/example/sugarbowl.html');
+    const stream = fs.createReadStream(`${__dirname}/example/sugarbowl.html`);
     stream.on('error', done);
     stream.pipe(parser(parse, function(err, status) {
-      var expected = {
-        'Lincoln Express': 'closed',
-        'Judah Express': 'closed',
-        'Disney Express': 'closed',
+      const expected = {
+        'Lincoln Express': 'scheduled',
+        'Judah Express': 'scheduled',
+        'Disney Express': 'scheduled',
         'Summit Chair': 'closed',
-        'Christmas Tree Express': 'closed',
-        'Crow\'s Peak': 'closed',
-        'Jerome Hill Express': 'closed',
-        'Village Tow': 'closed',
-        'White Pine (Beginner)': 'closed',
-        'Flume Carpet': 'closed',
-        'Nob Hill (Beginner)': 'closed',
-        'Gondola': 'closed'
+        'Christmas Tree Express': 'scheduled',
+        'Crow\'s Peak': 'scheduled',
+        'Jerome Hill Express': 'scheduled',
+        'Village Tow': 'scheduled',
+        'White Pine (Beginner)': 'scheduled',
+        'Flume Carpet': 'scheduled',
+        'Nob Hill (Beginner)': 'scheduled',
+        'Gondola': 'open',
+        'Village Kids Carpet': 'scheduled'
       };
       assert.deepEqual(status, expected);
       done(err);
