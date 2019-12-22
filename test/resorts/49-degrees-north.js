27e42951cef663e1c5c19257ad65163fe117f03c
Damian Krzeminski <pirxpilot@furkot.com>
2019-12-22 10:28:18 +0100

    fix 49 Degree North

--- test/resorts/49-degrees-north.js
+++ test/resorts/49-degrees-north.js
@@ -1,25 +1,25 @@
-var should = require('should');
-var fs = require('fs');
-var parser = require('../../lib/lifts/parser');
-var parse = require('../../lib/lifts/parse')('49-degrees-north');
+const assert = require('assert');
+const fs = require('fs');
+const parser = require('../../lib/lifts/parser');
+const parse = require('../../lib/lifts/parse')('49-degrees-north');
 
 /*global describe, it */
 describe('parse 49-degrees-north', function() {
 
   it('should return lift status', function(done) {
-    var stream = fs.createReadStream(__dirname + '/example/49-degrees-north.html');
+    const stream = fs.createReadStream(`${__dirname}/example/49-degrees-north.html`);
     stream.on('error', done);
     stream.pipe(parser(parse, function(err, status) {
-      var expected = {
-        '#1 Bonanza': 'closed',
-        '#2 Grubstake': 'closed',
-        '#3 Payday': 'closed',
-        '#4 West Basin': 'closed',
-        '#5 Sunrise Basin': 'closed',
-        'Hobbit Surface Lift': 'closed'
+      const expected = {
+        'Chair 1': 'open',
+        'Chair 2': 'closed',
+        'Chair 3': 'open',
+        'Chair 4': 'open',
+        'Chair 5': 'open',
+        'Chair 6': 'closed',
+        'Chair 7': 'open'
       };
-      should.exist(status);
-      status.should.eql(expected);
+      assert.deepEqual(status, expected);
       done(err);
     }));
   });
