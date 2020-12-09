eaae7ff503486e959f72e92022c09b6805217dd9
Damian Krzeminski <pirxpilot@furkot.com>
2020-12-09 18:11:11 +0100

    fix Attitash, Wildcat, Mount Snow
    they are now Vail resorts
    fixes #35

--- test/resorts/attitash.js
+++ test/resorts/attitash.js
@@ -1,15 +1,13 @@
 const lifts = require('../lifts');
 
 lifts('attitash', 'html', {
-  'L-1 Top Notch Double': 'closed',
-  'L-10 Snowtow': 'closed',
-  'L-11 Kachina Triple': 'closed',
-  'L-12 Flying Bear D-Quad': 'closed',
-  'L-2 Flying Yankee D-Quad': 'closed',
-  'L-3 Summit Triple': 'closed',
-  'L-4 East Borvig Double': 'closed',
-  'L-5 West Borvig Double': 'closed',
-  'L-6 Learning Center Triple': 'closed',
-  'L-7 Snowbelt': 'closed',
-  'L-8 Abenaki Quad': 'closed'
+  'Kachina': 'closed',
+  'Flying Bear Express': 'open',
+  'Flying Yankee': 'closed',
+  'Summit Triple': 'closed',
+  'East Double Double': 'closed',
+  'West Double Double': 'closed',
+  'Learning Center': 'closed',
+  'Snowbelt': 'closed',
+  'Abenaki Quad': 'open'
 });
