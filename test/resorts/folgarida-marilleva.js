ce0e1d5e5e6c9fc68b0410baf281a7c61f51887a
Damian Krzeminski <pirxpilot@furkot.com>
2021-10-01 18:44:31 +0200

    add Folgarida Marilleva

---
+++ test/resorts/folgarida-marilleva.js
@@ -0,0 +1,28 @@
+const lifts = require('../lifts');
+
+lifts('folgarida-marilleva', 'html', {
+  'Folgarida': 'closed',
+  'Malghet Aut 2': 'closed',
+  'Casina': 'closed',
+  'Marilleva 900': 'closed',
+  'Ometto': 'closed',
+  'Albare\'': 'closed',
+  'Biancaneve': 'closed',
+  'Marilleva': 'closed',
+  'Doss della Pesa': 'closed',
+  'Belvedere': 'closed',
+  'Panciana': 'closed',
+  'Sghirlat': 'closed',
+  'Mastellina': 'closed',
+  'Bassetta': 'closed',
+  'Ottava (solo serale)': 'closed',
+  'Daolasa 1': 'open',
+  'Daolasa 2': 'open',
+  'Spolverino': 'closed',
+  'Brenzi': 'closed',
+  'Vigo': 'closed',
+  'Malghette': 'closed',
+  'Orso Bruno': 'closed',
+  'Bamby': 'closed',
+  'Malghet Aut 1': 'closed'
+});
