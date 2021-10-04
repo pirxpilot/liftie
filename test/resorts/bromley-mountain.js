527fa6ac8173fb9c76c175ae0a7b75c9bba14d05
Damian Krzeminski <pirxpilot@furkot.com>
2021-10-04 07:01:31 +0200

    fix Bromley resort

--- test/resorts/bromley-mountain.js
+++ test/resorts/bromley-mountain.js
@@ -1,13 +1,12 @@
 const lifts = require('../lifts');
 
 lifts('bromley-mountain', 'html', {
- '#1 Sun Mountain Express': 'closed',
- '#2 Plaza Chairlift': 'closed',
- '#4 Sun Chairlift': 'closed',
- '#5 Alpine Chairlift': 'closed',
- '#6 East Meadow Chairlift': 'closed',
- '#7 Star Carpet': 'closed',
- '#8 Lord\'s Prayer T-Bar': 'closed',
- '#10 Blue Ribbon Quad': 'closed',
- '#11 Kids Carpet': 'closed'
+ 'Sun Mountain Express': 'closed',
+ 'Plaza Chairlift': 'closed',
+ 'Sun Chairlift': 'closed',
+ 'Star Carpet': 'closed',
+ 'East Meadow Chairlift': 'closed',
+ 'Alpine Chairlift': 'closed',
+ 'Blue Ribbon Quad': 'closed',
+ 'Lord\'s Prayer T-Bar': 'closed'
 });
