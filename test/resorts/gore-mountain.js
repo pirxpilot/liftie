e4667d8cac00dd149422e01e7af5c0562202feb7
Damian Krzeminski <pirxpilot@furkot.com>
2021-10-06 07:58:26 +0200

    fix Gore Mountain resort

--- test/resorts/gore-mountain.js
+++ test/resorts/gore-mountain.js
@@ -1,18 +1,10 @@
 const lifts = require('../lifts');
 
 lifts('gore-mountain', 'html', {
-  'Northwoods Gondola': 'closed',
-  'Topridge Triple': 'closed',
-  'Straight Brook Quad': 'closed',
-  'Sunway Chair': 'closed',
-  'Adirondack Express II': 'closed',
-  'Burnt Ridge Quad': 'closed',
-  'High Peaks Chair': 'closed',
-  'North Quad': 'closed',
-  'J-Bar': 'closed',
-  'Bear Cub': 'closed',
-  'Greenway Conveyor': 'closed',
-  'The Snow Train': 'closed',
-  'Village Chair': 'closed',
-  'Hudson Chair': 'closed'
+  'Gondola Skyride': 'closed',
+  'Chairlift Skyride': 'closed',
+  'Hiking': 'closed',
+  'Downhill Mountain Biking': 'closed',
+  'Mountaintop Labyrinth': 'closed',
+  'Disc Golf': 'closed'
 });
