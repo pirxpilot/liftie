c8141beb199f9c88a87eb36521bc92b592e71498
Damian Krzeminski <pirxpilot@furkot.com>
2021-12-02 11:25:38 +0100

    fix Boyne resorts

--- test/resorts/loon.js
+++ test/resorts/loon.js
@@ -2,13 +2,13 @@ const lifts = require('../lifts');
 
 lifts('loon', 'html', {
   'North Peak Express Quad': 'open',
-  'Lincoln Express Quad': 'open',
-  'Tote Road Connector': 'open',
-  'Kancamagus Express Quad': 'open',
-  'Kissin\' Cousin Double': 'open',
-  'Sarsaparilla Carpet': 'open',
-  'Gondola': 'open',
-  'Seven Brothers Triple': 'open',
+  'White Mountain Express Gondola': 'open',
+  'Lincoln Express Quad': 'closed',
+  'Tote Road Connector': 'closed',
+  'Kancamagus 8': 'closed',
+  'Kissin\' Cousin Double': 'closed',
+  'Sarsaparilla Carpet': 'closed',
   'East Basin Double': 'closed',
-  'Little Sister Double': 'closed'
+  'Little Sister Double': 'closed',
+  'Seven Brothers Triple': 'closed'
 });
