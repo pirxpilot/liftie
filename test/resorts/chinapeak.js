b8a7cad330015b0993ec4f9eee249fc6c07c444f
Damian Krzeminski <pirxpilot@furkot.com>
2021-12-07 08:02:49 +0100

    fix China Peak resort

--- test/resorts/chinapeak.js
+++ test/resorts/chinapeak.js
@@ -1,16 +1,13 @@
 const lifts = require('../lifts');
 
 lifts('chinapeak', 'html', {
-  'Chair 1': 'scheduled',
-  'Chair 2': 'open',
-  'Chair 3': 'closed',
+  'Chair 1': 'closed',
+  'Chair 2': 'closed',
   'Chair 4': 'closed',
   'Chair 5': 'closed',
-  'Chair 6': 'open',
+  'Chair 6': 'closed',
   'Chair 7': 'closed',
   'Kids Carpet': 'closed',
   'Boulder Carpet': 'closed',
-  'Juniper Carpet': 'open',
-  'Tubing Hill': 'closed',
-  'T-Bar': 'closed'
+  'Juniper Carpet': 'closed',
 });
