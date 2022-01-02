1534cf9bc41934d4d79c509cb4f2aa58ca509c21
Matthew Hall <matthall28@gmail.com>
2022-01-02 16:29:43 +0000

    add Mt Seymour

---
+++ test/resorts/mt-seymour.js
@@ -0,0 +1,8 @@
+const lifts = require('../lifts');
+
+lifts('mt-seymour', 'html', {
+  'Lodge Chair': 'open',
+  'Mystery Peak Express': 'open',
+  'Brockton Chair': 'open',
+  'Goldie Magic Carpet': 'open',
+});
