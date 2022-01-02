c8a0dc8534b8faa545566ebdb31f6315539bfc96
Matthew Hall <matthall28@gmail.com>
2022-01-02 00:34:14 +0000

    add Canada Olympic Park

---
+++ test/resorts/canada-olympic-park.js
@@ -0,0 +1,10 @@
+const lifts = require('../lifts');
+
+lifts('canada-olympic-park', 'html', {
+  '88 Lift Express': 'open',
+  'Legacy Chairlift': 'open',
+  'Progression Carpet': 'open',
+  'Gold Carpet': 'open',
+  'Silver Carpet': 'open',
+  'Bronze Carpet': 'open',
+});
