08b889e26ed7af9f63da7ea472c9c81bb350adb2
Matthew Hall <matthall28@gmail.com>
2022-01-03 20:02:03 -0800

    Adding Sasquatch Mountain
    Adding Sasquatch Mountain

---
+++ test/resorts/sasquatch-mountain.js
@@ -0,0 +1,8 @@
+const lifts = require('../lifts');
+
+lifts('sasquatch-mountain', 'xml', {
+  'A- Yeti Cruiser Quad': 'closed',
+  'B- Whistlepunk Double': 'open',
+  'Littlefoot Carpet Lift': 'open',
+  'D- Sasquatch Triple': 'open',
+});
