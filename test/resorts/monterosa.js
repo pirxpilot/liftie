6e29b9f4010f49c3912ca0bb8b9e0b1530070d33
Damian Krzeminski <pirxpilot@furkot.com>
2023-10-14 09:42:41 +0200

    add Monterosa resort
    based on #58

---
+++ test/resorts/monterosa.js
@@ -0,0 +1,28 @@
+const lifts = require('../lifts');
+
+lifts('monterosa', 'html', {
+  'CREST-ALPE OSTAFA III': 'closed',
+  'ALPE OSTAFA-COLLE SAREZZA II': 'closed',
+  'LAGO CIARCERIO-ALPE BELVEDERE': 'closed',
+  'FRACHEY-ALPE CIARCERIO': 'open',
+  'ALPE MANDRIA': 'closed',
+  'BETTAFORCA': 'closed',
+  'CHAMPOLUC-CREST': 'open',
+  'FONTANEY 1': 'closed',
+  'SANTANNA-BETTAFORCA': 'open',
+  'STAFAL-SANTANNA': 'open',
+  'STAFAL-GABIET': 'open',
+  'GABIET-LAGO': 'closed',
+  'BEDEMIE-SEEHORN': 'closed',
+  'PUNTA JOLANDA': 'closed',
+  'GABIET-PASSO DEI SALATI': 'open',
+  'PASSO DEI SALATI-INDREN': 'open',
+  'PIANALUNGA-CIMALEGNA-SALAT': 'closed',
+  'ALAGNA-PIANALUNGA': 'closed',
+  'PIANALUNGA-BOCCHETTA': 'closed',
+  'WOLD 2': 'closed',
+  'TAPIS ROULANT WOLD': 'closed',
+  'TAPIS ROULANT PIANALUNGA': 'closed',
+  'CIMALEGNA-SALATI': 'closed'
+
+});
