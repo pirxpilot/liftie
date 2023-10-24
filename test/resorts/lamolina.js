4d25cdfb5671bea788eb05c756c565a25fe94911
Federico Zivolo <federico.zivolo@gmail.com>
2023-10-24 09:40:12 +0200

    add La Molina resort
    closes #68

---
+++ test/resorts/lamolina.js
@@ -0,0 +1,8 @@
+const lifts = require('../lifts');
+
+lifts('lamolina', 'html', {
+  'CT Johan Cruyff': 'closed',
+  'TSD Cap de Comella': 'closed',
+  'TCB Cadí-Moixeró - Part 1': 'closed',
+  'TCB Cadí-Moixeró - Part II': 'closed',
+});
