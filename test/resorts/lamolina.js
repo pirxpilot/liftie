0d20edc214cf079812e1e1af296b1137f53cda8c
Damian Krzeminski <pirxpilot@furkot.com>
2023-10-24 15:36:32 +0200

    fixup! feat: La Molina

--- test/resorts/lamolina.js
+++ test/resorts/lamolina.js
@@ -1,7 +1,7 @@
 const lifts = require('../lifts');
 
 lifts('lamolina', 'html', {
-  'CT Johan Cruyff	': 'closed',
+  'CT Johan Cruyff': 'closed',
   'TSD Cap de Comella': 'closed',
   'TCB Cadí-Moixeró - Part 1': 'closed',
   'TCB Cadí-Moixeró - Part II': 'closed',
