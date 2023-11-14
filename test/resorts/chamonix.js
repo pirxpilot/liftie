bf2beef49acb741013d7810ede77c480fd029c63
Federico Zivolo <federico.zivolo@gmail.com>
2023-11-14 14:50:40 +0100

    feat: add chamonix

---
+++ test/resorts/chamonix.js
@@ -0,0 +1,24 @@
+const lifts = require('../lifts');
+
+lifts('chamonix', 'html', {
+  'Tc Plan Joran': 'closed',
+  'Tsd Tabe': 'closed',
+  'Tk Marmottons': 'closed',
+  'Acces Flegere': 'closed',
+  'Tc Charamillon': 'closed',
+  'Tc Vallorcine': 'closed',
+  'Tsd Autannes': 'closed',
+  'Tph Bellevue': 'closed',
+  'Tc Prarion': 'closed',
+  "Tph Plan De L'aiguille": 'closed',
+  'Tph Aiguille Du Midi': 'closed',
+  Ascenseur: 'closed',
+  'Train Montenvers Mer De Glace': 'closed',
+  'Tc Mer De Glace': 'closed',
+  'Train Fayet-col De Voza': 'closed',
+  'Train Col De Voza-bellevue': 'closed',
+  Planards: 'closed',
+  'Luge De Chamonix': 'hold',
+  "Parc D'attractions": 'hold',
+  'Tc Flegere': 'closed',
+});
