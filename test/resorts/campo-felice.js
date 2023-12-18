2ff587af33bcd9b6483713dc35aef8552acba17e
Federico Zivolo <federico.zivolo@gmail.com>
2023-12-18 14:30:59 +0100

    feat: Campo Felice

---
+++ test/resorts/campo-felice.js
@@ -0,0 +1,19 @@
+const lifts = require('../lifts');
+
+lifts('campo-felice', 'html', {
+  'Seggiovia Brecciara': 'closed',
+  'Seggiovia Monterotondo': 'closed',
+  'Seggiovia Campo Felice': 'closed',
+  'Seggiovia Cisterna': 'closed',
+  'Seggiovia Nibbio': 'closed',
+  'Seggiovia Scuola': 'closed',
+  'Seggiovia Cerchiare': 'closed',
+  'Skilift Pista Dick': 'closed',
+  'Seggiovia Colle Sinistro': 'closed',
+  'Seggiovia Colle Destro': 'closed',
+  'Manovia Baby Campo Felice': 'closed',
+  'Tappeto Innamorati': 'closed',
+  'Skilift Snow Park': 'closed',
+  'Tappeto scuola (Snow Tubing)': 'closed',
+  'Tappeto Scuola Campo Felice': 'closed',
+});
