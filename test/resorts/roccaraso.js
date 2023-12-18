02c01e71c32645418cf07f5411bac5d3cf840939
Federico Zivolo <federico.zivolo@gmail.com>
2023-12-18 14:39:38 +0100

    feat: Roccaraso

---
+++ test/resorts/roccaraso.js
@@ -0,0 +1,26 @@
+const lifts = require('../lifts');
+
+lifts('roccaraso', 'html', {
+  'Valle delle Gravare': 'closed',
+  'Gravare di Sotto': 'closed',
+  Macchione: 'closed',
+  Aremogna: 'closed',
+  Pallottieri: 'open',
+  'Campo Scuola': 'closed',
+  'ValleVerde 1': 'closed',
+  'ValleVerde 2': 'closed',
+  'Aremogna Crete Rosse': 'closed',
+  'Crete Rosse 1': 'closed',
+  'Pino Solitario': 'closed',
+  'Del Pratelletto': 'closed',
+  'Monte Pratello': 'closed',
+  Valloncello: 'closed',
+  Prato: 'closed',
+  Vallone: 'closed',
+  'Pratello Crete Rosse': 'closed',
+  'Lago d’Avoli Orso': 'closed',
+  Pizzalto: 'closed',
+  'Paradiso 1 e 2': 'closed',
+  'Gravare-ToppeTes': 'closed',
+  'Pizzalto-Gravare': 'closed',
+});
