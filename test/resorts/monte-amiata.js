62ef5538d9a27cab53750fbac884946804c62346
Federico Zivolo <federico.zivolo@gmail.com>
2023-12-18 13:54:46 +0100

    feat: Monte Amiata

---
+++ test/resorts/monte-amiata.js
@@ -0,0 +1,22 @@
+const lifts = require('../lifts');
+
+lifts('monte-amiata', 'html', {
+  'Campo Scuola Vetta': 'closed',
+  'Crocicchio Dedo': 'closed',
+  Canalgrande: 'closed',
+  'Canalgrande Cantore': 'closed',
+  'Canalgrande Macinaie': 'closed',
+  'Campo scuola Macinaie': 'closed',
+  Direttissima: 'closed',
+  'Direttissima (Tratto finale)': 'closed',
+  Panoramica: 'closed',
+  Bellaria: 'closed',
+  'Campo scuola Contessa': 'closed',
+  'Anello Marsigliana (2km Sci Nordico)': 'closed',
+  'Seggiovia Cantore': 'closed',
+  'Seggiovia Macinaie': 'closed',
+  'Sciovia Crocicchio': 'closed',
+  'Sciovia Jolly': 'closed',
+  'Sciovia Asso di Fiori': 'closed',
+  'Sciovia Bellaria': 'closed',
+});
