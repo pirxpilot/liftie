49e4c55fba334d654f0112b7aefd411227f51378
Federico Zivolo <federico.zivolo@gmail.com>
2023-12-18 14:18:29 +0100

    feat: Ovindoli

---
+++ test/resorts/ovindoli.js
@@ -0,0 +1,16 @@
+const lifts = require('../lifts');
+
+lifts('ovindoli', 'html', {
+  Nome: 'scheduled',
+  'B - Topolino 1': 'open',
+  'E - Dolce Vita - Magnola': 'closed',
+  'C - Topolino 2': 'closed',
+  'A - Le Fosse - Monte Arso': 'closed',
+  'D - Snow Park': 'closed',
+  'F - Capanna Brin - Daino': 'closed',
+  'G - Anfiteatro - Coccinella': 'closed',
+  'M - Fonte Fredda - Monte Freddo': 'closed',
+  'H - Campetto degli Alpini - Monte Freddo': 'closed',
+  'I - Kids': 'closed',
+  'L - Belvedere': 'closed',
+});
