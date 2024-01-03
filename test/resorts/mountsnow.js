886ccf96a937bb52fd70041f20c22c9392590f3a
Damian Krzeminski <pirxpilot@furkot.com>
2024-01-03 15:14:22 +0100

    fix mountsnow test

--- test/resorts/mountsnow.js
+++ test/resorts/mountsnow.js
@@ -1,24 +1,23 @@
 const lifts = require('../lifts');
 
-lifts.skip('mountsnow', 'html', {
-  'Apollo': 'closed',
+lifts('mountsnow', 'html', {
+  Apollo: 'open',
   'Bear Trap': 'closed',
   'Bluebird Express': 'open',
-  'Canyon Express': 'closed',
-  'Challenger': 'open',
+  'Canyon Express': 'open',
+  Challenger: 'open',
+  'Covered Bridge': 'closed',
   'Discovery Shuttle': 'open',
   'Ego Alley': 'closed',
-  'Gemini': 'closed',
+  Gemini: 'closed',
   'Grand Summit Express': 'closed',
-  'Grommet': 'closed',
+  Grommet: 'closed',
   'Heavy Metal': 'closed',
-  'Mercury': 'closed',
-  'Mixing Bowl': 'closed',
-  'Nitro Express': 'closed',
-  'Outpost': 'closed',
-  'Seasons': 'closed',
-  'Sunbrook': 'closed',
-  'Sundance': 'closed',
-  'Tumbleweed': 'closed',
-  'Voyager': 'closed'
+  Mercury: 'closed',
+  'Nitro Express': 'open',
+  Outpost: 'closed',
+  Seasons: 'closed',
+  'Sunbrook Express': 'open',
+  'Sundance Express': 'open',
+  Voyager: 'closed'
 });
