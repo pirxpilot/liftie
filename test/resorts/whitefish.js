9ca6bf5ba5911c9a014ec318161b1a0df9f1a9cf
Jin Choi <jsc@alum.mit.edu>
2024-02-29 15:38:26 -0500

    Add Whitefish Mountain Resort

---
+++ test/resorts/whitefish.js
@@ -0,0 +1,20 @@
+const lifts = require('../lifts');
+
+lifts('whitefish', 'html', {
+  'Chair 1 - Big Mountain Express': 'open',
+  'Chair 2 - Swift Creek Express': 'open',
+  'Chair 3 - Tenderfoot': 'open',
+  'Chair 4 - Snow Ghost Express': 'open',
+  'Chair 5 - East Rim': 'closed',
+  'Chair 6 - Village': 'open',
+  'Chair 7 - Big Creek Express': 'open',
+  'Chair 8 - Hellroaring': 'open',
+  'Chair 9 - Easy Rider': 'open',
+  'Chair 10 - Bad Rock': 'closed',
+  'Chair 11 - Flower Point': 'open',
+  'Chair 12 - Elk Highlands': 'open',
+  'T-Bar 1 - Heritage': 'closed',
+  'T-Bar 2 - Bigfoot': 'closed',
+  'Big Easy Carpet': 'open',
+  '2 Easy Carpet': 'closed',
+});
