2d4aad68ea076754dc9f18970329d421cff02041
Jin Choi <jsc@alum.mit.edu>
2024-02-29 15:41:11 -0500

    Add Bogus Basin

---
+++ test/resorts/bogusbasin.js
@@ -0,0 +1,14 @@
+const lifts = require("../lifts");
+
+lifts("bogusbasin", "html", {
+  "Deer Point Express": "open",
+  "Morning Star Express": "open",
+  "Superior Express": "open",
+  "Showcase Chair": "closed",
+  "Bitterroot Chair": "closed",
+  "Pine Creek Express": "open",
+  "Coach Chairlift": "open",
+  "Easy Rider Carpet": "open",
+  "Discovery Carpet": "open",
+  "Explorer Carpet": "open",
+});
