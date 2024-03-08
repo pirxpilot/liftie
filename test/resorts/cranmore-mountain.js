493802792a0f96beee68161e7b669d4fe392dca3
Jin Choi <jsc@alum.mit.edu>
2024-03-08 12:09:02 -0500

    Update for Cranmore Mountain

--- test/resorts/cranmore-mountain.js
+++ test/resorts/cranmore-mountain.js
@@ -1,12 +1,15 @@
-const lifts = require('../lifts');
+const lifts = require("../lifts");
 
-lifts('cranmore-mountain', 'html', {
-  'Skimobile Express': 'open',
-  'C-More Double': 'open',
-  'South Quad': 'open',
-  'Flying Carpet': 'closed',
-  'Lookout Triple': 'closed',
-  'Magic Carpet': 'open',
-  'Schneider Triple': 'closed',
-  'Tubing Carpet': 'open'
+lifts("cranmore-mountain", "html", {
+  "C-More Double": "open",
+  "Flying Carpet": "closed",
+  "Lookout Triple": "closed",
+  "Magic Carpet": "open",
+  "Schneider Triple": "closed",
+  "Skimobile Express": "open",
+  "South Quad": "open",
+  "Giant Swing": "open",
+  "Mountain Coaster": "open",
+  "Soaring Eagle": "open",
+  "Tubing Park": "open",
 });
