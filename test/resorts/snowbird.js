0b70721a27e7fc9a474bc7af7b1bad5423c9a5c6
Damian Krzeminski <pirxpilot@furkot.com>
2024-12-12 16:38:35 -0700

    fix Snowbird resort

--- test/resorts/snowbird.js
+++ test/resorts/snowbird.js
@@ -1,6 +1,18 @@
 const lifts = require('../lifts');
 
-lifts('snowbird', 'html', {
-  'Aerial Tram': 'open',
-  'Chickadee': 'closed'
+lifts('snowbird', 'json', {
+  'Baby Thunder': 'closed',
+  'Aerial Tram': 'scheduled',
+  'Gadzoom': 'scheduled',
+  'Peruvian': 'closed',
+  'Chickadee': 'open',
+  'Mineral Basin': 'closed',
+  'Baldy': 'closed',
+  'Wilbere': 'open',
+  'Peruvian Tunnel': 'closed',
+  'Gad 2': 'closed',
+  'Little Cloud': 'closed',
+  'Chickadee Conveyor': 'scheduled',
+  'Baby Thunder Conveyor': 'closed',
+  'Mid-Gad': 'closed'
 });
