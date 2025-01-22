ba5bc683749e101d24768f5045ae48a7f2de5305
Bret Barker <bret@abitrandom.net>
2025-01-21 19:55:53 -0500

    add Pleasant Mountain, formerly Shawnee Peak

---
+++ test/resorts/pleasant-mountain.js
@@ -0,0 +1,10 @@
+const lifts = require('../lifts');
+
+lifts('pleasant-mountain', 'json', {
+  "Li'l Pine Beginner Carpet": 'open',
+  'Pine Quad': 'open',
+  'Rabbit Run': 'open',
+  'Snow Pine Carpet': 'closed',
+  'Summit Express': 'open',
+  'Sunnyside Triple': 'closed'
+});
