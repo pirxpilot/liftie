9cd3b52a21402a0c80b2e4871ee131fc56a107c6
Damian Krzeminski <pirxpilot@furkot.com>
2025-03-08 13:46:03 -0700

    format code

--- test/resorts/mtpeter.js
+++ test/resorts/mtpeter.js
@@ -3,7 +3,7 @@ const lifts = require('../lifts');
 lifts('mtpeter', 'html', {
   'Carpet Lift (Snowbasin)': 'scheduled',
   'Comet': 'open',
-  'Ol\' Pete': 'closed',
+  "Ol' Pete": 'closed',
   'The Patriot': 'open',
   'High Point Carpet': 'open',
   'Valley View Carpet': 'open',
