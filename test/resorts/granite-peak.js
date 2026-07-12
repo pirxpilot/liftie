9301a64850fc1684493b8b49045dc49f14b276f8
Orlando Castiglioni <orlando@orlandocc.me>
2026-07-12 08:29:58 -0400

    resort: add granite-peak (https://www.skigranitepeak.com/mountain-info/mountain-report)

---
+++ test/resorts/granite-peak.js
@@ -0,0 +1,11 @@
+import lifts from '../lifts/index.js';
+
+lifts('granite-peak', 'html', {
+  'Comet 6-Pack': 'closed',
+  Blitzen: 'closed',
+  Santa: 'closed',
+  'Frosty Carpet': 'closed',
+  'Blizzard Carpet': 'closed',
+  'Cupid Quad': 'closed',
+  'Dasher Quad': 'closed'
+});
