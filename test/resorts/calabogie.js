1b2db333689d4de20107a943749a59c84d5af2c4
Orlando Castiglioni <orlando@orlandocc.me>
2026-07-12 08:08:37 -0400

    resort: add calabogie (https://calabogie.com/trail-status/)

---
+++ test/resorts/calabogie.js
@@ -0,0 +1,26 @@
+import lifts from '../lifts/index.js';
+
+lifts('calabogie', 'html', {
+  'Upper Ole K & P': 'open',
+  'Lower Ole K & P': 'open',
+  'Shantyman': 'open',
+  'Fiddler': 'open',
+  "Shannahan's": 'open',
+  'Beginner Bowl': 'open',
+  'Progression': 'open',
+  'Barrett Chute': 'open',
+  "O'Brien's": 'open',
+  'Jack Bone': 'open',
+  "Dillon's Dip": 'open',
+  'Bear Paw': 'closed',
+  "Bailey's": 'closed',
+  'Causeway': 'open',
+  'Black Donald': 'open',
+  'Juniper Junction': 'open',
+  "Al's Alley": 'open',
+  'Whistlin Paddy': 'open',
+  'Lower Finnegan': 'open',
+  'Buckshot': 'open',
+  'Face of Tiger': 'open',
+  'Mini Park': 'open'
+});
