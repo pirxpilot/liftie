490312a3e90905891d44deb446dda907eb41a2d6
Orlando Castiglioni <orlando@orlandocc.me>
2026-07-12 08:05:23 -0400

    resort: add donner-ski-ranch (https://www.donnerskiranch.com/liftstatus)

---
+++ test/resorts/donner-ski-ranch.js
@@ -0,0 +1,12 @@
+import lifts from '../lifts/index.js';
+
+lifts('donner-ski-ranch', 'html', {
+  'Chair 1': 'open',
+  'Chair 2': 'open',
+  'Chair 3': 'open',
+  'Chair 4': 'open',
+  'Chair 5': 'closed',
+  'Chair 6': 'open',
+  'Ski Moving Carpet': 'open',
+  'Tubing Hill': 'open'
+});
