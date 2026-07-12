c522c0452d7dd63d9d18c53f8b1d66b611a38008
Orlando Castiglioni <orlando@orlandocc.me>
2026-07-12 08:36:41 -0400

    resort: add west-mountain (https://westmountain.com/trail-conditions/)

---
+++ test/resorts/west-mountain.js
@@ -0,0 +1,10 @@
+import lifts from '../lifts/index.js';
+
+lifts('west-mountain', 'html', {
+  'Apex Triple Chairlift': 'open',
+  'Face Quad Chairlift': 'closed',
+  'West Express Triple Chairlift': 'closed',
+  'Learning Area Surface Lift': 'closed',
+  'Tubing Park Surface Lift': 'closed',
+  'Learning Area Rope Tow for Kiddie TP access': 'closed'
+});
