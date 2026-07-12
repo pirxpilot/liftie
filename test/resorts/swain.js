127d1292b815cbea3b206c8de0b15720d8db238d
Orlando Castiglioni <orlando@orlandocc.me>
2026-07-12 08:26:35 -0400

    resort: add swain (https://swain.com/trail-report/)

---
+++ test/resorts/swain.js
@@ -0,0 +1,9 @@
+import lifts from '../lifts/index.js';
+
+lifts('swain', 'html', {
+  'Quad 1': 'closed',
+  'Quad 2': 'open',
+  'Quad 3': 'open',
+  'Double Chair': 'closed',
+  'Magic Carpet': 'open'
+});
